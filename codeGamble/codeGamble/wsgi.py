10cd2fcab1acfd2a5646b8f3625b9d93b920b2a8
Hemant Jadon <hemantjadon.hemu@gmail.com>
2016-03-10 04:47:12 +0530

    test

--- codeGamble/codeGamble/wsgi.py
+++ codeGamble/codeGamble/wsgi.py
@@ -1,17 +1,16 @@
+"""
+WSGI config for codeGamble project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
 import os
-import time
-import traceback
-import signal
-import sys
+
 from django.core.wsgi import get_wsgi_application
 
-try:
-    application = get_wsgi_application()
-    
-except Exception:
-    
-    # Error loading applications
-    if 'mod_wsgi' in sys.modules:
-        traceback.print_exc()
-        os.kill(os.getpid(), signal.SIGINT)
-        time.sleep(2.5)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codeGamble.settings")
+
+application = get_wsgi_application()
