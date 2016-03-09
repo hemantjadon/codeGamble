a9ebed5f271d6ef193973511b32cff2d8a282f88
Hemant Jadon <hemantjadon.hemu@gmail.com>
2016-03-09 18:40:18 +0530

    Testing environment variables

--- codeGamble/codeGamble/wsgi.py
+++ codeGamble/codeGamble/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codeGamble.settings")
 
+os.environ["DJANGO_SETTINGS_MODULE"] = "codeGamble.settings"
 application = get_wsgi_application()
